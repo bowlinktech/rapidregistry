652377392c058046d4064e81972bfd26d27bf575
Chad <chadmccue05@gmail.com>
2014-12-15 15:16:33 -0500

    - db updates

--- src/main/java/com/bowlink/rr/model/User.java
+++ src/main/java/com/bowlink/rr/model/User.java
@@ -19,22 +19,22 @@ import org.springframework.format.annotation.DateTimeFormat;
 @Entity
 @Table(name = "USERS")
 public class User {
-    
+
     @Transient
     private Long timesloggedIn = null;
-    
+
     @Transient
     private String staffType = null;
-    
+
     @Transient
     private List<String> registries = null;
-    
+
     @Transient
     private String encryptedId = null;
-    
+
     @Transient
     private String encryptedSecret = null;
-    
+
     @Transient
     private String password;
 
@@ -56,22 +56,15 @@ public class User {
     @Column(name = "LASTNAME", nullable = true)
     private String lastName;
 
-    /**
-    @NotEmpty
-    @NoHtml
-    @Column(name = "PASSWORD", nullable = false)
-    private String password;
-    **/
-    
     @NotEmpty
     @Email
     @NoHtml
     @Column(name = "EMAIL", nullable = false)
     private String email;
-    
+
     @Column(name = "TYPEID", nullable = false)
     private Integer typeId = 0;
-    
+
     @Column(name = "ROLEID", nullable = false)
     private int roleId = 2;
 
@@ -81,37 +74,37 @@ public class User {
     @DateTimeFormat(pattern = "dd/MM/yyyy hh:mm:ss")
     @Column(name = "DATECREATED", nullable = true)
     private Date dateCreated = new Date();
-    
+
     @NoHtml
     @Column(name = "RESETCODE", nullable = true)
     private String resetCode = null;
-    
+
     @Column(name = "LASTLOGGEDIN", nullable = true)
     private Date lastloggedIn = null;
-    
+
     @Column(name = "randomSalt", nullable = true)
-    private byte [] randomSalt;
+    private byte[] randomSalt;
 
     @Column(name = "encryptedPw", nullable = true)
-    private byte [] encryptedPw;
-    
+    private byte[] encryptedPw;
+
     public byte[] getRandomSalt() {
-		return randomSalt;
-	}
+        return randomSalt;
+    }
 
-	public void setRandomSalt(byte[] randomSalt) {
-		this.randomSalt = randomSalt;
-	}
+    public void setRandomSalt(byte[] randomSalt) {
+        this.randomSalt = randomSalt;
+    }
 
-	public byte[] getEncryptedPw() {
-		return encryptedPw;
-	}
+    public byte[] getEncryptedPw() {
+        return encryptedPw;
+    }
 
-	public void setEncryptedPw(byte[] encryptedPw) {
-		this.encryptedPw = encryptedPw;
-	}
+    public void setEncryptedPw(byte[] encryptedPw) {
+        this.encryptedPw = encryptedPw;
+    }
 
-	public int getId() {
+    public int getId() {
         return id;
     }
 
@@ -158,7 +151,7 @@ public class User {
     public void setCreatedBy(int createdBy) {
         this.createdBy = createdBy;
     }
-    
+
     public String getEmail() {
         return email;
     }
@@ -186,7 +179,7 @@ public class User {
     public String getresetCode() {
         return resetCode;
     }
-    
+
     public void setresetCode(String resetCode) {
         this.resetCode = resetCode;
     }
@@ -246,5 +239,5 @@ public class User {
     public void setLastloggedIn(Date lastloggedIn) {
         this.lastloggedIn = lastloggedIn;
     }
-    
+
 }
