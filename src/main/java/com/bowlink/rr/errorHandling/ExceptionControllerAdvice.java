efc5747a223ce1edff539ec22e56f542ca786162
Grace Chan <gchan@Olive.local>
2019-02-21 13:56:36 -0500

    updated email to read from properties file

--- src/main/java/com/bowlink/rr/errorHandling/ExceptionControllerAdvice.java
+++ src/main/java/com/bowlink/rr/errorHandling/ExceptionControllerAdvice.java
@@ -50,8 +50,8 @@ public class ExceptionControllerAdvice {
         try {
         mailMessage messageDetails = new mailMessage();
         
-        messageDetails.settoEmailAddress("information@health-e-link.net");
-        messageDetails.setfromEmailAddress("information@health-e-link.net");
+        messageDetails.settoEmailAddress(myProps.getProperty("admin.email"));
+        messageDetails.setfromEmailAddress("support@health-e-link.net");
         messageDetails.setmessageSubject("Exception Error " + myProps.getProperty("server.identity"));
         
         StringBuilder sb = new StringBuilder();
